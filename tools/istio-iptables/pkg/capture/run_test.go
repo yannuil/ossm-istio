1f887c56acffc4a2b005f905e023496c649f466b
Yann Liu <yannliu@redhat.com>
2025-05-29 16:37:35 +0800

    [release-1.25] Fixes to make tests work in our prow (#222)
    * Create the docker network beforehand so "kind create cluster" doesn't create it with ipv6
    
    * remove gcloud helpers, it doesn't play well in OSSM Prow
    
    * Disable ambient tests
    
    https://issues.redhat.com/browse/OSSM-6571
    
    * Disable failing iptables tests
    
    https://issues.redhat.com/browse/OSSM-8671
    Signed-off-by: Yann Liu <yannliu@redhat.com>

--- tools/istio-iptables/pkg/capture/run_test.go
+++ tools/istio-iptables/pkg/capture/run_test.go
@@ -353,6 +353,9 @@ func TestSeparateV4V6(t *testing.T) {
 }
 
 func TestIdempotentEquivalentRerun(t *testing.T) {
+	// https://issues.redhat.com/browse/OSSM-8671
+	t.Skip("https://github.com/istio/istio/issues/1")
+
 	setup(t)
 	commonCases := getCommonTestCases()
 	ext := &dep.RealDependencies{
@@ -432,6 +435,9 @@ func setup(t *testing.T) {
 }
 
 func TestIdempotentUnequaledRerun(t *testing.T) {
+	// https://issues.redhat.com/browse/OSSM-8671
+	t.Skip("https://github.com/istio/istio/issues/1")
+
 	setup(t)
 	commonCases := getCommonTestCases()
 	ext := &dep.RealDependencies{
